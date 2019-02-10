bba66870bf471db4304831ffc4a00251b00a7096
vacoseelar <>
2019-02-10 14:44:03 -0500

    changes made for Spring boot version 2.0.5

--- eaweb/src/main/java/gov/va/ea/web/Application.java
+++ eaweb/src/main/java/gov/va/ea/web/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
